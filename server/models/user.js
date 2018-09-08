9613c9475799ac0a27ceb395c5e0abd427b01a42
caidi <caidicaidi123@gmail.com>
2018-09-08 13:58:17 +0800

    generate jason web token

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,7 @@
+require('dotenv').load();
 import mongoose from 'mongoose';
 import validator from 'validator';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -25,8 +27,22 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true
+    },
+    token: {
+        type: String
     }
 });
+
+UserSchema.method.generateToken = function() {
+    const user = this;
+    const access = 'auth';
+    const uid = user._id.toHexString();
+
+    const token = jwt.sign({access, uid}, process.env.NODE_SECRET);
+    user.token = token;
+
+    return token;
+};
 
 const User = mongoose.model('User', UserSchema);
 
