e020633d013d65c1777300c9e2e197b042945fc6
caidicaidi123 <caidicaidi123@gmail.com>
2018-09-13 14:38:29 +0800

    initial commit

---
+++ server/models/store.js
@@ -0,0 +1,13 @@
+require('dotenv').load();
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+const StoreSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    open: {
+        type: Boolean,
+    }
+});
